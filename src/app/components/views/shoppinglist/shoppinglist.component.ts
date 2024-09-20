754b1381ccecb70ed5abe699f88cb62bd569968c
Janari Kasar <janari.kasar@gmail.com>
2024-09-20 19:43:01 +0300

    Basic shopping list querying and updated dev ip

--- src/app/components/views/shoppinglist/shoppinglist.component.ts
+++ src/app/components/views/shoppinglist/shoppinglist.component.ts
@@ -1,11 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { StatusService } from '../../../services/status.service';
+import { ShoppinglistService } from '../../../services/shoppinglist.service';
+import { CommonModule } from '@angular/common';
+import { CardComponent } from '../../card/card.component';
 
 @Component({
   selector: 'app-calendar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, CardComponent],
   templateUrl: './shoppinglist.component.html'
 })
-export class ShoppinglistComponent {
+export class ShoppinglistComponent implements OnInit {
  
+  list!: any[];
+
+  constructor(private shoppinglistService: ShoppinglistService){
+  }
+
+  ngOnInit(): void {
+    this.shoppinglistService.getList().subscribe((result) => {
+      this.list = result;
+      console.log(this.list);
+    })
+  }
+
 }
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiBaseUrl: 'http://192.168.0.140:8081/api'
+    apiBaseUrl: 'http://bee.local:8081/api'
 };
