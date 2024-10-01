0915fcd51caaaec0f5c725db6e61b8f8117940e4
Siret Eller <sireteller@gmail.com>
2024-10-01 09:52:33 +0300

    chore(food): rename food-modal to meal-selection-modal

--- src/app/components/views/food/food.component.ts
+++ src/app/components/views/food/food.component.ts
@@ -1,25 +1,30 @@
 import { Component } from '@angular/core';
 import { MealPlanComponent } from '../../meal-plan/meal-plan.component';
 import { MealCardSelectionBtnComponent } from '../../meal-card-selection-btn/meal-card-selection-btn.component';
-import { FoodModalComponent } from "../../food-modal/food-modal.component";
+import { FoodModalComponent } from '../../meal-selection-modal/meal-selection-modal.component';
 import { CommonModule } from '@angular/common';
 import { Meal } from '../../../models/food.model';
-import { MealEditModal } from "../../meal-edit-modal/meal-edit-modal.component";
+import { MealEditModal } from '../../meal-edit-modal/meal-edit-modal.component';
 
 @Component({
   selector: 'app-food',
   standalone: true,
-  imports: [CommonModule, MealPlanComponent, MealCardSelectionBtnComponent, FoodModalComponent, MealEditModal],
+  imports: [
+    CommonModule,
+    MealPlanComponent,
+    MealCardSelectionBtnComponent,
+    FoodModalComponent,
+    MealEditModal,
+  ],
   templateUrl: './food.component.html',
   styleUrl: './food.component.css',
 })
 export class FoodComponent {
-
   openedModalName: string | null = null;
   editingMeal: Meal | null = null;
 
   openFoodModal($event: string) {
-      this.openedModalName = $event;
+    this.openedModalName = $event;
   }
 
   closeFoodModal() {
@@ -32,9 +37,9 @@ export class FoodComponent {
 
   closeMealEditModal(updated: boolean) {
     this.editingMeal = null;
-    if (updated) { // TODO refresh food modal instead of close some how
+    if (updated) {
+      // TODO refresh food modal instead of close some how
       this.closeFoodModal();
     }
   }
-  
 }
